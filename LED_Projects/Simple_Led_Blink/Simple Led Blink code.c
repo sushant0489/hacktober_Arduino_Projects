e30a4d414c33c06fcb389d833434081347c3a35f
Sushant Prasad <114470086+sushant0489@users.noreply.github.com>
2024-10-02 13:41:07 +0530

    Create Simple Led Blink code.c
    // This program runs the LED connected to GPIO PIN 11 using Arduino UNO

---
+++ LED_Projects/Simple_Led_Blink/Simple Led Blink code.c
@@ -0,0 +1,17 @@
+// This program runs the LED connected to GPIO PIN 11
+
+#define LED_BUILTIN 11  // the anode of LED is connected to GPIO pin 11 which is named as LED_BUILTIN
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
